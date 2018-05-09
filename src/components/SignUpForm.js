c18cbce02fa5164dce3ca7299d53cac5418e3811
Kristen Goehringer <kgoehringer@gmail.com>
2018-05-09 17:49:20 +0200

    Added CSS Grid to sign up form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -5,7 +5,7 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import postNewUser from '../api/signUpForm'
 
-//once REDUX is setup, need to add action to handleSubmit to change signedIn to TRUE
+//TODO once REDUX is setup, need to add action to handleSubmit to change signedIn to TRUE
 
 class SignUpForm extends Component {
   constructor(props) {
@@ -40,11 +40,11 @@ class SignUpForm extends Component {
     e.preventDefault();
     postNewUser(this.state)
 
-    // DISPATCH AN ACTION TO SET SIGNED IN TRUE
+    //TODO DISPATCH AN ACTION TO SET SIGNED IN TRUE
   };
   render() {
     return <div className="signUpForm">
-        <div className="fuck">
+        <div>
           <h2 className="signUpForm__header">
             Sign up and start grilling today!
           </h2>
@@ -53,67 +53,68 @@ class SignUpForm extends Component {
             with Grillber.
           </p>
           <form onSubmit={this.handleSubmit}>
-            <div className="row">
-                <div>
-                  <label htmlFor="firstName" className="signUpForm__label">First name</label>
-                  <input type="text" id="firstName" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="lastName" className="signUpForm__label">Last name</label>
-                  <input type="text" id="lastName" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="email" className="signUpForm__label">Email address</label>
-                  <input type="text" id="email" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="dob" className="signUpForm__label">Date of birth</label>
-                  
-                </div>
-                <div>
-                  <label htmlFor="street1" className="signUpForm__label">Street address</label>
-                  <input type="text" id="street1" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="street2" className="signUpForm__label">
-                    Street address 2 <span className="signUpForm__helper">(optional)</span>
-                  </label>
-                  <input type="text" id="street2" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="state" className="signUpForm__label">State</label>
-									{this.state.currentStates.map(state => <button
-										className={this.state.state === state ? "signUpForm__buttons active" : "signUpForm__buttons"}
-										type="button"
-										value={state}
-										key={state}
-										onClick={this.handleClick}>{state}
-										</button> )}
-
-                </div>
-                <div>
-                  <label htmlFor="zipCode" className="signUpForm__label">Zip code</label>
-                  <input type="text" id="zipCode" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="phone" className="signUpForm__label">Phone number</label>
-                  <input type="text" id="phone" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="password" className="signUpForm__label">
-                    Password <span className="signUpForm__helper">(min 6 characters)</span>
-                  </label>
-                  <input type="text" id="password" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <div>
-                  <label htmlFor="password2" className="signUpForm__label">
-                    Password <span className="signUpForm__helper">(please re-enter)</span>
-                  </label>
-                  <input type="text" id="password2" className="signUpForm__input" onChange={this.handleChange} />
-                </div>
-                <button type="submit" className="btn btn--action signUpButton">
-                  Sign up
-                </button>
+            <div className="signUpForm__wrapper">
+              <div>
+                <label htmlFor="firstName" className="signUpForm__label">First name</label>
+                <input type="text" id="firstName" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="lastName" className="signUpForm__label">Last name</label>
+                <input type="text" id="lastName" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="email" className="signUpForm__label">Email address</label>
+                <input type="text" id="email" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="dob" className="signUpForm__label">Date of birth</label>
+                <input type="text" className="signUpForm__input" />
+              </div>
+              <div>
+                <label htmlFor="street1" className="signUpForm__label">Street address</label>
+                <input type="text" id="street1" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="street2" className="signUpForm__label">
+                  Street address 2 <span className="signUpForm__helper">(optional)</span>
+                </label>
+                <input type="text" id="street2" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div className="signUpForm__state">
+                <label htmlFor="state" className="signUpForm__label">State</label>
+								<div>
+								{this.state.currentStates.map(state => <button
+                  className={this.state.state === state ? "signUpForm__buttons active" : "signUpForm__buttons"}
+                  type="button"
+                  value={state}
+                  key={state}
+                  onClick={this.handleClick}>{state}
+                  </button> )}
+								</div>
+              </div>
+              <div>
+                <label htmlFor="zipCode" className="signUpForm__label">Zip code</label>
+                <input type="text" id="zipCode" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="phone" className="signUpForm__label">Phone number</label>
+                <input type="text" id="phone" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="password" className="signUpForm__label">
+                  Password <span className="signUpForm__helper">(min 6 characters)</span>
+                </label>
+                <input type="text" id="password" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <div>
+                <label htmlFor="password2" className="signUpForm__label">
+                  Password <span className="signUpForm__helper">(please re-enter)</span>
+                </label>
+                <input type="text" id="password2" className="signUpForm__input" onChange={this.handleChange} />
+              </div>
+              <button type="submit" className="btn btn--action signUpButton">
+                Sign up
+              </button>
             </div>
           </form>
         </div>
@@ -123,16 +124,79 @@ class SignUpForm extends Component {
 
 export default SignUpForm;
 
-
-// <DatePicker
-//   dateFormat="YYYY/MM/DD"
-//   selected={this.state.date}
-//   showYearDropdown
-//   scrollableYearDropdown
-//   yearDropdownItemNumber={99}
-//   onChange={this.handleChange}
-// />;
-
-//<button className="signUpForm__buttons" type="button" value="MA" onClick={this.handleClick}>MA</button>
-//<button className="signUpForm__buttons" type="button" value="NY" onClick={this.handleClick}>NY</button>
-//<button className="signUpForm__buttons" type="button" value="PA" onClick={this.handleClick}>PA</button>
+// render() {
+// 	return <div className="signUpForm">
+// 		<div className="fuck">
+// 			<h2 className="signUpForm__header">
+// 				Sign up and start grilling today!
+// 			</h2>
+// 			<p className="signUpForm__copy">
+// 				Please provide the requested information to create your account
+// 				with Grillber.
+// 			</p>
+// 			<form onSubmit={this.handleSubmit}>
+// 				<div className="row">
+// 					<div>
+// 						<label htmlFor="firstName" className="signUpForm__label">First name</label>
+// 						<input type="text" id="firstName" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="lastName" className="signUpForm__label">Last name</label>
+// 						<input type="text" id="lastName" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="email" className="signUpForm__label">Email address</label>
+// 						<input type="text" id="email" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="dob" className="signUpForm__label">Date of birth</label>
+//
+// 					</div>
+// 					<div>
+// 						<label htmlFor="street1" className="signUpForm__label">Street address</label>
+// 						<input type="text" id="street1" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="street2" className="signUpForm__label">
+// 							Street address 2 <span className="signUpForm__helper">(optional)</span>
+// 						</label>
+// 						<input type="text" id="street2" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="state" className="signUpForm__label">State</label>
+// 						{this.state.currentStates.map(state => <button
+// 							className={this.state.state === state ? "signUpForm__buttons active" : "signUpForm__buttons"}
+// 							type="button"
+// 							value={state}
+// 							key={state}
+// 							onClick={this.handleClick}>{state}
+// 						</button> )}
+//
+// 					</div>
+// 					<div>
+// 						<label htmlFor="zipCode" className="signUpForm__label">Zip code</label>
+// 						<input type="text" id="zipCode" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="phone" className="signUpForm__label">Phone number</label>
+// 						<input type="text" id="phone" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="password" className="signUpForm__label">
+// 							Password <span className="signUpForm__helper">(min 6 characters)</span>
+// 						</label>
+// 						<input type="text" id="password" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<div>
+// 						<label htmlFor="password2" className="signUpForm__label">
+// 							Password <span className="signUpForm__helper">(please re-enter)</span>
+// 						</label>
+// 						<input type="text" id="password2" className="signUpForm__input" onChange={this.handleChange} />
+// 					</div>
+// 					<button type="submit" className="btn btn--action signUpButton">
+// 						Sign up
+// 					</button>
+// 				</div>
+// 			</form>
+// 		</div>
+// 	</div>;
